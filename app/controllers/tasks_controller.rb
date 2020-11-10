de5056876bd7348959d2f7e823fed9626855135d
Barkley Santo <barkley.santo@gmail.com>
2020-11-10 16:28:50 -0600

    CRUD operations complete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,55 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i[show edit update destroy]
+
   def index
-    @tasks = Tasks.all
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.update!(task_params)
+      redirect_to @task
+    else
+      render :edit
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update!(task_params)
+      redirect_to @task
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to root_path
+  end
+
+  private
+
+  def task_params
+    params.require('task').permit(:title, :details)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Verb Path      Controller#Action
+  root to: 'tasks#index'
+  # get all tasks
   get '/tasks', to: 'tasks#index'
+  # create a task
+  get 'tasks/new', to: 'tasks#new'
+  # get one task by its id
+  get '/tasks/:id', to: 'tasks#show', as: :task
+  # create new task
+  post '/tasks', to: 'tasks#create'
+  # update task
+  get '/tasks/:id/edit', to: 'tasks#edit'
+  patch '/tasks/:id', to: 'tasks#update'
+  # delete task
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
